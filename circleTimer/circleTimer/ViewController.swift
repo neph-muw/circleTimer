99af20a52a0f1cf90cf52558835b8a463f14bc4a
nostradupus <romanmykitchak@gmail.com>
2018-08-03 12:32:51 +0200

    First commit

---
+++ circleTimer/circleTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  circleTimer
+//
+//  Created by Roman Mykitchak on 03/08/2018.
+//  Copyright © 2018 Roman Mykitchak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
